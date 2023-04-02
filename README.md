d4c5cefeb8984dcb4b7c602bce63b66d4e7c2851
Julia Aparecida Moreira <julia.aparecida25@gmail.com>
2023-04-02 12:09:53 -0300

    Upload dos primeiros códigos
    Upload dos primeiros códigos desenvolvidos

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# space-tourism-website-main
- Site desenvolvido a partir do layuot disponibilizado no site https://www.frontendmentor.io/challenges
+## Site Space Tourism Website
+----
+Este site está sendo desenvolvido com base no desafio do site Frontend Mentor, onde são disponibilizados layouts de site. 
+
+<b>Link do site: </b> https://www.frontendmentor.io/home
+
+### Tecnologias utilizadas para a sua criação
+- HTML
+- CSS
+- JavaScript 
---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(".link-one").click(function() {
+    $("#d1").show();
+    $("#d2,#d3,#d4").hide();
+});
+$(".link-two").click(function() {
+    $("#d2").show();
+    $("#d1,#d3,#d4").hide();
+});
+$(".link-three").click(function() {
+    $("#d3").show();
+    $("#d1,#d2,#d4").hide();
+});
+$(".link-four").click(function() {
+    $("#d4").show();
+    $("#d1,#d2,#d3").hide();
+});
+  
+$('.btn-links-planets').click(function(){
+    $('.btn-links-planets').removeClass('selected')
+    $(this).addClass('selected');
+});
